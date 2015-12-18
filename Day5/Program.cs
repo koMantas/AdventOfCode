6ff2fc2ec74b6a71e0f031a6b2b21e6143065896
KoMantas <m.kontrimas@yahoo.com>
2015-12-18 23:12:46 +0200

    Day 5 finished

---
+++ Day5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(@"D:\Dropbox\Projects\C#\AdventOfCode\Day5\input5day.txt");
+            int numOfNiceWords = 0;
+            //regex for aeiou  ^.*[aeiou].*[aeiou].*[aeiou].*$
+            //regex for double letter (.)\1
+            //regex not match ab|cd|pq|xy
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (Regex.IsMatch(line, @"^.*[aeiou].*[aeiou].*[aeiou].*$") &&
+                    Regex.IsMatch(line, @"(.)\1") &&
+                         !Regex.IsMatch(line, "ab|cd|pq|xy"))
+                {
+                    numOfNiceWords++;
+                }
+            }
+            reader.Close();
+            Console.WriteLine("(1part)Num of nice words: " + numOfNiceWords);
+            numOfNiceWords = 0;
+
+            //read from begining again
+            reader = new StreamReader(@"D:\Dropbox\Projects\C#\AdventOfCode\Day5\input5day.txt");
+
+            //(..).*\1 regex chech for a pair of any two letters that appears at least twice in the string without overlapping
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (Regex.IsMatch(line, @"(..).*\1") &&
+                    Regex.IsMatch(line, @"(.).\1"))
+                {
+                    numOfNiceWords++;
+                }
+            }
+            reader.Close();
+            Console.WriteLine("(2part)Num of nice words: " + numOfNiceWords);
+        }
+    }
+}
--- Day5/Properties/AssemblyInfo.cs
+++ Day5/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Day 4_2")]
+[assembly: AssemblyTitle("Day5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Day 4_2")]
+[assembly: AssemblyProduct("Day5")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c32db0a1-d9d9-4fb5-b189-f23c97c3d095")]
+[assembly: Guid("9e70a1f9-e0ed-4fdc-a78d-7e42c29a2349")]
 
 // Version information for an assembly consists of the following four values:
 //
